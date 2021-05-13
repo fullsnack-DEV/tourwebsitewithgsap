756f770759e10b96d397a67db3da1e0f91c42b51
fullsnack-DEV <pratikpatre2019@gmail.com>
2021-05-13 17:20:36 +0530

    fixed reponsive issue

--- src/Components/Main-sec2.jsx
+++ src/Components/Main-sec2.jsx
@@ -4,45 +4,45 @@ export const Main2 = () => {
   return (
     <div className="sub">
       <div className="sub__container">
-        <div className="sub__heading">
-          <div className="sub__heading-1">
-            <div className="sub__heading-circle" />
-            <h2>
-              Listen and <br /> Download Music
-            </h2>
-            <p>
-              In our app you can download the music both in the app <br />
-              itself on your Smartphone. Try this feature today.
-            </p>
-          </div>
-          <div className="sub__heading-2">
-            <div className="circle" />
-            <div className="sub__heading-text">
-              <h3>Downloading in the app</h3>
-              <p>
-                Owenrs of the reqular version of Move Music <br /> can only
-                download music in the app and listen to <br /> it without the
-                internet
-              </p>
-            </div>
-          </div>
-          <div className="sub__heading-3">
-            <div className="circle" />
-            <div className="sub__heading-text">
-              <h3>
-                Downloading to <br /> your Smartphone
-              </h3>
-              <p>
-                Owenrs of the reqular version of Move Music <br /> can only
-                download music in the app and listen to <br /> it without the
-                internet
-              </p>
-            </div>
-          </div>
-        </div>
         <div className="sub__img">
-          <div className="sub__img--circle" />
-          <img src={Image2} alt="saly" />
+          <div className="sub__img-circle" />
+          <img src={Image2} alt="" className="sub__img-svg" />
+        </div>
+        <div className="sub__heading">
+          <div className="sub__heading-container">
+            <div className="sub__heading-container--1">
+              <div className="sub__heading-circle" />
+              <h2>
+                Listen and <br /> Download Music
+              </h2>
+              <p>
+                In our app you can download the music both in the app itself{" "}
+                <br /> and on on your Smartphone. Try this feature today.
+              </p>
+            </div>
+            <div className="sub__heading-container--2">
+              <div className="sub__heading-container-circle" />
+
+              <div className="sub__heading-container-text">
+                <h3>Downloading in the app</h3>
+                <p>
+                  We analyze your media library and create <br /> Playlist that
+                  match yout mood and music <br /> preferences.
+                </p>
+              </div>
+            </div>
+            <div className="sub__heading-container--3">
+              <div className="sub__heading-container-circle" />
+              <div className="sub__heading-container-text">
+                <h3>Downloding to your smartphone</h3>
+                <p>
+                  You can import your music from other music <br /> Platforms
+                  and not waste time adding music match yout mood and music{" "}
+                  <br /> Manually.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
